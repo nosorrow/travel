8d50fc3000cd273fcaca57286de5f7ab9e61605b
nosorrow <plamenorama@gmail.com>
2023-02-26 13:33:11 +0200

    2023-02-26

--- resources/views/site-details.blade.php
+++ resources/views/site-details.blade.php
@@ -35,7 +35,15 @@
     <div>
       <img src="{{url('img/baba-vida-1.jpg')}}" class="w-100 mb-2 sm:mb-6" alt="bv">
       <div>
-        <h2 class="text-4xl font-semibold text-gray-900 py-6">
+        <div class="flex items-center text-gray-400">
+           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                stroke="currentColor" class="flex w-4 h-4">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z" />
+              <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z" />
+            </svg>
+            <span class="ml-2">обл. Видин</span>
+        </div>
+        <h2 class="text-4xl font-semibold text-gray-900 pb-6">
           Открийте средновековието
         </h2>
         <p class="py-4 text-gray-700">
